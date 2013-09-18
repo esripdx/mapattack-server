69e2c752f56ec066ea6e6cc82c356ab59e3199cb
Jerry Sievert <code@legitimatesounding.com>
2013-09-18 13:00:51 -0700

    add web server

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var socket = require('./lib/socket');
+var socket = require('./lib/socket'),
+    web    = require('./lib/web'),
+    config = require('./config.json');
+
 
 var msgpack = require('msgpack');
 
 
-socket.createSocket(5309, function (err, server) {
+socket.createSocket(config.udp_port, function (err, server) {
   server.on("message", function (msg, rinfo) {
     var decoded = msgpack.unpack(msg);
     console.log("server got: a message from " +
       rinfo.address + ":" + rinfo.port, decoded);
+  });
+});
+
+web.createWebServer(config.http_port, function (err, server) {
+  server.on("request", function (request, response) {
+    console.log("http request: " + request.url);
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.end("404 error");
   });
 });
